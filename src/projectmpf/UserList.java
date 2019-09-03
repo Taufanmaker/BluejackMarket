ebc9a00fe4b47f1eba5bbc5948a8b5c3a48b8d7f
rifansetiawan <37042039+rifansetiawan@users.noreply.github.com>
2019-09-03 15:06:42 +0700

    Create UserList.java

---
+++ src/projectmpf/UserList.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectmpf;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author ASUS
+ */
+public class UserList {
+    public static Vector<String> username = new Vector<>();
+    public static Vector<String> password = new Vector<>();
+    
+    public static Vector<String> itemName = new Vector<>();
+    public static Vector<Integer> itemPrice = new Vector<>();
+    public static Vector<String> imagePath = new Vector<>();
+}
